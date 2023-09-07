4dd9b16137cddae7d1b1904d7fa773ab2d50180e
Willian Agostini <willian.agostini@gmail.com>
2023-09-06 22:01:07 -0300

    added initial tests

---
+++ tests/index.spec.ts
@@ -0,0 +1,43 @@
+import app from "../src/index";
+import httpMocks from "node-mocks-http";
+
+describe("routerTree", () => {
+  it("Find route by path with params", async () => {
+    let value = false;
+
+    app.get("/test/:id", () => {
+      value = true;
+    });
+
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/test/1",
+      params: {
+        id: 42,
+      },
+    });
+
+    const response = httpMocks.createResponse();
+    await app.execute(request, response);
+    expect(value).toBe(true);
+  });
+
+  it("Find route by path with params", async () => {
+    let params: any = {};
+    app.post("/test/:id", (req) => {
+      params = req.params
+    });
+    
+    const request = httpMocks.createRequest({
+      method: "POST",
+      url: "/test/1",
+      params: {
+        id: 42,
+      },
+    });
+
+    const response = httpMocks.createResponse();
+    await app.execute(request, response);
+    expect(params?.id).toBe('1');
+  });
+});
---
+++ tests/routerTree.spec.ts
@@ -0,0 +1,45 @@
+import { RouterTree } from "../src/routerTree";
+
+describe("routerTree", () => {
+  it("Find route by path with params", async () => {
+    const routeTree = new RouterTree();
+    routeTree.addRoute("/test/:id", () => {});
+
+    const routerInfo = routeTree.findRoute("/test/1");
+    expect(routerInfo?.params?.id).toEqual("1");
+  });
+
+  it("Find route by path with multiple params", async () => {
+    const routeTree = new RouterTree();
+    routeTree.addRoute("/test/:id/:another", () => {});
+
+    const routerInfo = routeTree.findRoute("/test/1/2");
+    expect(routerInfo?.params?.id).toEqual("1");
+    expect(routerInfo?.params?.another).toEqual("2");
+  });
+
+  it("Find route by path with multiple params", async () => {
+    const routeTree = new RouterTree();
+    routeTree.addRoute("/state/:id/city/:another", () => {});
+
+    const routerInfo = routeTree.findRoute("/state/1/city/2");
+    expect(routerInfo?.params?.id).toEqual("1");
+    expect(routerInfo?.params?.another).toEqual("2");
+  });
+
+  it("Find undefined route", async () => {
+    const routeTree = new RouterTree();
+    routeTree.addRoute("/state/:id/city/:another", () => {});
+
+    const routerInfo = routeTree.findRoute("/state/");
+    expect(routerInfo?.handler).toBeFalsy();
+  });
+
+  it("Find route by path with searchParams", async () => {
+    const routeTree = new RouterTree();
+    routeTree.addRoute("/state", () => {});
+
+    const routerInfo = routeTree.findRoute("/state?id=1");
+    expect(routerInfo?.searchParams.get("id")).toBe("1");
+  });
+});
